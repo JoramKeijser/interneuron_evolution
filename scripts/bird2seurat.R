257b295fd2502da126e67379f88b577eedbe9b5c
Joram <joramkeijser@gmail.com>
2022-12-22 13:17:59 +0100

    Change data dir and savedir

--- scripts/bird2seurat.R
+++ scripts/bird2seurat.R
@@ -2,34 +2,28 @@
 library(dplyr)
 library(Seurat)
 library(SeuratDisk)
-SAVE_RAW = FALSE
 
-bird <- read.table("./data/raw/bird/HVC_RA_RNA_counts.csv", header=TRUE, sep=",")
+datadir <- "~/Dropbox/scRNAseq_data/Colquitt21/"
+savedir <- "./data/Colquitt21/"
+bird <- read.table(paste0(datadir, "HVC_RA_RNA_counts.csv"), header=TRUE, sep=",")
 counts <- bird %>% select(!c(cell,UMAP_1, UMAP_2, X, nCount_RNA, nFeature_RNA, percent.mito,
                              species, position2, cluster_int_sub2, orig.ident))
 meta.data <- bird %>% select(c(cell,UMAP_1, UMAP_2, nCount_RNA, nFeature_RNA, percent.mito,
                              species, position2, cluster_int_sub2, orig.ident))
-if (SAVE_RAW){
-  write.table(meta.data, "./data/raw/bird/metadata.csv", sep=",")
-  write.table(counts, "./data/raw/bird/counts.csv", sep=",")  
-}
 
-# Create Seurat
+# Prep data for seurat object. 
 # "Row names in the metadata need to match the column names of the counts matrix"
 # Metadata cells x vars. Counts: genes x cells
 counts <- t(counts) # cells x genes -> genes x cells
 colnames(counts) <- meta.data$cell
-
 rownames(meta.data) <- meta.data$cell
 meta.data <-meta.data %>% select(!cell)
+# Create Seurat
 bird <- CreateSeuratObject(counts = counts, project = "bird", 
                            meta.data = meta.data, 
                            min.cells = 1, min.features = 1)
 bird # 18342 features across 29497 samples within 1 assay 
-SaveH5Seurat(bird, "./data/raw/bird/bird", overwrite=TRUE)
-
-# SCTransform
-bird <- SCTransform(bird, vars.to.regress = "percent.mito", 
-                    verbose = TRUE, variable.features.n = 3000)
-SaveH5Seurat(bird, "./data/raw/bird/birdSCT", overwrite=TRUE)
-
+SaveH5Seurat(bird, paste0(savedir, "bird"), overwrite=TRUE)
+# Also save in scanpy/H5ad format
+Convert(paste0(savedir, paste0("bird", ".h5seurat")), 
+        dest = "h5ad", overwrite=TRUE)
