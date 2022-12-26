aa2c3730e4f0e8a9b4f356548bb2af8bb48f70d1
Joram <joramkeijser@gmail.com>
2022-12-26 21:28:53 +0100

    Add jackstraw

--- scripts/human.R
+++ scripts/human.R
@@ -26,13 +26,36 @@ human <- FindVariableFeatures(object = human) # HVGs
 human <- ScaleData(human) #
 human <- RunPCA(object = human) 
 human <- FindNeighbors(object = human)
-human <- FindClusters(object = human, resolution = 0.4)
+human <- FindClusters(object = human, resolution = 0.2)
 human <- RunUMAP(object = human, dims=1:30)
+
+# to do
+human <- JackStraw(human, num.replicate = 100)
+human <- ScoreJackStraw(human, dims = 1:20)
+JackStrawPlot(human, dims = 1:20)
 
 # Viz. 
 DimPlot(object = human, reduction = "umap")
 FeaturePlot(human, c("SST", "PVALB", "VIP", "LAMP5"))
+FeaturePlot(human, c("SST", "CBLN4", "ELFN1"))
 
-# PCA plot
+# PCA plot - consistent with what we saw in Scanpy?
 DimPlot(object = human, reduction = "pca")
 FeaturePlot(human, c("SST", "PVALB", "VIP", "LAMP5"), reduction = "pca")
+
+# # find markers for every cluster compared to all remaining cells, report only the positive
+# ones
+markers <- FindAllMarkers(human, only.pos = TRUE, min.pct = 0.25,
+                          logfc.threshold = 0.25)
+# 2 per cluster
+markers %>%
+  group_by(cluster) %>%
+  slice_max(n = 2, order_by = avg_log2FC)
+
+# select top 10 per cluster
+markers %>%
+  group_by(cluster) %>%
+  top_n(n = 10, wt = avg_log2FC) -> top10
+DoHeatmap(human, features = top10$gene) + NoLegend()
+
+
