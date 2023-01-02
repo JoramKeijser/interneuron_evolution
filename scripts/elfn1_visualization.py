838b62c97a4ed5a40ebca4fdee2f2c62fe6bc0a9
Joram <joramkeijser@gmail.com>
2023-01-02 12:27:34 +0100

    Add Cbln4 viz

--- scripts/elfn1_visualization.py
+++ scripts/elfn1_visualization.py
@@ -10,12 +10,12 @@ sc.settings.figdir = "./figures/"
 
 def main():
     seed = 1029
-    
+    data_dir = "../tPC_data/"
     # Upper lim. mapped to color. Higher expressions are clipped. 
-    max_expr = {'tosches': 2.2, 'colquitt': 4.2, "tasic": 1.5, "bakken": 1.25}
+    max_expr = {'tosches': 2.2, 'colquitt': 4.2, "tasic": 1.75, "bakken": 1.25}
     for dataset in ['tasic', 'bakken', 'tosches', 'colquitt']:
         print(f"Processing {dataset}")
-        adata = ad.read_h5ad(f"./data/anndata/{dataset}_raw.h5ad")
+        adata = ad.read_h5ad(f"{data_dir}{dataset}_raw.h5ad")
         # Focus on 5 canonical mammalian classes. 
         keep_subclasses = ['Pvalb', 'Sst', 'Lamp5', 'Sncg', 'Vip']
         adata.obs['subclass'] = adata.obs['subclass'].astype("category")
@@ -23,28 +23,26 @@ def main():
         if 'Serpinf1' in adata.obs['subclass'].cat.categories:
             adata.obs['subclass'] = adata.obs['subclass'].cat.remove_categories(['Serpinf1'])
             adata.obs.loc[adata.obs['subclass'].isna(),'subclass'] = 'Vip'
-        # Remove Meis2
+        # Only keep canonical subclasses (remove e.g. Meis2)
         extra_categories = [subclass for subclass in adata.obs['subclass'].cat.categories if subclass not in keep_subclasses]
-        #for subclass in extra_categories:
         adata = adata[~adata.obs['subclass'].isin(extra_categories)]
         missing_categories = [subclass for subclass in keep_subclasses if subclass not in adata.obs['subclass'].cat.categories]
         adata.obs['subclass'] = adata.obs['subclass'].cat.add_categories(missing_categories)
-        print(adata.obs['subclass'].cat.categories)
-        #if dataset in ['tosches', 'colquitt']:
-        # Order by Elfn1 expression in mice
         adata = adata[adata.obs['subclass']!='Sncg']
+        # Order by Elfn1 expression in mice
         adata.obs['subclass'] = adata.obs['subclass'].cat.reorder_categories(['Sst', 'Vip', 'Pvalb', 'Lamp5'])
         #else:
         #    adata.obs['subclass'] = adata.obs['subclass'].cat.reorder_categories(['Sst', 'Vip', 'Sncg', 'Pvalb', 'Lamp5'])
 
-        # Normalize
+        # Normalize to log CP10K
         sc.pp.normalize_total(adata, 1e4)
         sc.pp.log1p(adata)
-        # Violin plot Elfn1
-        fig, ax = plt.subplots(figsize=(9,4))
-        sns.despine()
-        sc.pl.violin(adata, "Elfn1", "subclass", ax=ax, ylabel = "Elfn1 (log CP10K)", xlabel = "Subclass",
-                    save=f"_{dataset}.png", show=False)
+        # Violin plots Elfn1 amd Cbln4
+        for gene in ['Elfn1', 'Cbln4']:
+            fig, ax = plt.subplots(figsize=(9,4))
+            sns.despine()
+            sc.pl.violin(adata, gene, "subclass", ax=ax, ylabel = f"{gene} (log CP10K)", xlabel = "Subclass",
+                        save=f"_{gene}_{dataset}.png", show=False)
 
         # PCA on HVGs
         sc.pp.highly_variable_genes(adata)
@@ -56,8 +54,9 @@ def main():
         # Visualize UMAP
         sc.pl.umap(adata, color='subclass', legend_loc='on data', show=False,
                 frameon=False, title='Subclass', save=f"_subclass_{dataset}.png")
-        sc.pl.umap(adata, color='Elfn1', legend_loc='on data', show = False,
-                frameon=False, title='Elfn1', save=f"_elfn1_{dataset}.png", vmax=max_expr[dataset])
+        for gene in ['Elfn1', 'Cbln4', 'Reln', 'Calb2', 'Myh8']:
+            sc.pl.umap(adata, color=gene, legend_loc='on data', show = False,
+                frameon=False, title=gene, save=f"_{gene}_{dataset}.png", vmax=max_expr[dataset])
 
 
 if __name__ == "__main__":
