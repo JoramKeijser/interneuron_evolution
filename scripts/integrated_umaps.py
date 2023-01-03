4d719b0a9fc6ae49b1c9fdd1878b022151fe464a
Joram <joramkeijser@gmail.com>
2023-01-03 13:28:26 +0100

    Adds integrated umaps

--- scripts/integrated_umaps.py
+++ scripts/integrated_umaps.py
@@ -1,3 +1,4 @@
+# Visualize integrated datasets
 import numpy as np 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,15 +9,15 @@ from matplotlib.colors import ListedColormap
 def main():
     sns.set_context("poster")
     sns.set_palette("Set2")
-    data_dir = "./data/raw/"
+    data_dir = "./data/anndata/"
     fig_dir = "./figures/"
 
     for celltype in ['GABAergic', 'Glutamatergic']:
-        adata = ad.read_h5ad(f"{data_dir}mouse_bird_{celltype}_integrated.h5ad")
+        adata = ad.read_h5ad(f"{data_dir}mouse_bird_{celltype}_integrated_SCT.h5ad")
         fig, ax = plt.subplots()
         sc.pl.umap(adata, color='organism', frameon=False, title=f'{celltype} neurons', 
             ax=ax, legend_loc='None', show=False)
-        fig.savefig(f"{fig_dir}integrated_umap_{celltype}", dpi=300)
+        fig.savefig(f"{fig_dir}integrated_umap_{celltype}_SCT", dpi=300)
 
 
 if __name__ == "__main__":
