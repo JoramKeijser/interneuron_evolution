1aece7baed6a2fd911ccdffb5a272ca78b817039
Joram Keijser <joramkeijser@gmail.com>
2023-01-06 11:48:08 +0100

    Rm download from pipeline

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-# Run the entire pipeline
-
-# First download the data
-cd elfn1_evolution # assumes you're in the base dir
-./download_data.sh
-
-# Run the actual code
+# Run the entire pipeline. Assumes you're in the repo directory
 conda activate elfn1_env 
 ./scripts/preprocess.sh # Puts data into Seurat and AnnData files
 python ./scripts/visualize_expression.py # UMAPs and violin plots
