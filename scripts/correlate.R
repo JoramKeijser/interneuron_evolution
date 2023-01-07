1a88c054280d9caec1288e85fd696cf82d285ef9
Joram <joramkeijser@gmail.com>
2023-01-07 21:00:02 +0100

    add serpinf1 and sncg cluster

--- scripts/correlate.R
+++ scripts/correlate.R
@@ -1,13 +1,15 @@
 # Correlate gene expression between clusters
 source("./src/seurat_utils.R")
 
-for (cell_type in c("Glut", "GABA")){
-  # Load bird data
+for (cell_type in c( "GABA", "Glut")){
+  print(paste("Comparing cell type", cell_type))
+  print("Loading bird data")
   bird <- LoadH5Seurat("./data/seurat/bird.h5seurat") 
   bird # 218342 features across 29497 samples within 1 assay 
   # Select largest dataset: zebra finch (not bengalese)
   bird <- bird[, bird@meta.data$species == 'zf'] # 25392 samples within 1 assay 
   # Load mouse data
+  print("Loading mouse data")
   mouse <- LoadH5Seurat("./data/seurat/mouse.h5seurat")
   mouse # 42094 features across 15413 samples within 1 assay 
   
@@ -33,8 +35,9 @@ for (cell_type in c("Glut", "GABA")){
   Idents(bird) <- bird@meta.data$subclass
   Idents(mouse) <- mouse@meta.data$subclass
   
-  # For each dataset: marker genes 
+  print("Find marker genes: bird")
   top.bird.markers <- get_top_markers(bird) 
+  print("Find marker genes: mouse")
   top.mouse.markers <- get_top_markers(mouse) 
   
   # Select shared marker genes
@@ -47,21 +50,24 @@ for (cell_type in c("Glut", "GABA")){
   
   # Correlate
   C <- cor(mouse.avg, bird.avg) 
-  # Throw out un-matched songbird cells. 
   if (cell_type == "GABA"){
-    C <- C[!rownames(C) %in% c("Serpinf1", "Sncg"), !colnames(C) %in% c("Pre", "7", "8")]
+    # Throw out "subcortical" songbird cells. 
+    # !rownames(C) %in% c("Serpinf1", "Sncg")
+    C <- C[, !colnames(C) %in% c("Pre", "7", "8")]
     # Reorder
-    C <- C[c("Meis2", "Sst", "Pvalb", "Vip", "Lamp5"),]
+    C <- C[c("Meis2", "Sst", "Pvalb", "Vip", "Lamp5", "Serpinf1", "Sncg"),]
     }
   else if (cell_type == "Glut"){
     C <- C[, !(colnames(C) %in% c("NP", "CR"))]
     colnames(C) <- sapply(colnames(C), function(x){str_replace(x, "_Glut", "")})
   }
-  # Plot it
+  
+  fname <- paste0("./figures/corr_", cell_type, ".pdf")
+  print(paste("Visualize & save correlation matrix as", fname))
   fontsize <- 25
   # consistent color range for GABA and Glut:
   mat_breaks <- seq(-0.3, 0.4, length.out = 100) 
-  pdf(file = paste0("./figures/corr_", cell_type, ".pdf")) # save
+  pdf(file = fname) # save
   setHook("grid.newpage", 
           function() pushViewport(viewport(x=1,y=1, width=.9, height=0.7, 
                                            name="vp", just=c("right","top"))), action="prepend")
